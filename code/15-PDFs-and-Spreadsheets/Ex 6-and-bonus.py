d673f464671b9181abd2c4c15dc65428e8c40586
Minh Cuong Do <do006@gannon.edu>
2022-10-04 09:15:55 -0700

    Assignment for Oct 4

---
+++ code/15-PDFs-and-Spreadsheets/Ex 6-and-bonus.py
@@ -0,0 +1,49 @@
+"""
+Created on Oct 4 2022
+"""
+from audioop import avg
+import csv
+from encodings import utf_8
+
+import os
+
+
+
+os.chdir("D:/cognixia/learning-python/code/15-PDFs-and-Spreadsheets")
+# Example
+f=open("example.csv",encoding='utf_8')
+lines=f.readlines() # Store as list type
+for l in lines:
+    print(l)
+
+data = open("example.csv",encoding='utf-8') # TextIOWrapper type
+csv_data = csv.reader(data)
+
+
+with open("example.csv",encoding='utf-8') as f:
+    for line in f:
+        row = line.split(",")
+        print(row)
+# Does not understand \n as EOL in Windows
+
+# 6.1 cookbook
+# Reader
+with open("example.csv",encoding='utf-8') as f:
+    csv_data = csv.reader(f, delimiter=',') # delimiter can be set to other characters (\t for TSV)
+    header=next(csv_data)
+    # header = [ re.sub('[^a-zA-Z_]', '_', h) for h in next(f_csv) ] # Substitute invalid characters in header to underscore
+    print(header)
+    for row in csv_data:
+        print(row)
+        # print(row[1]) # Get all data in 2nd column
+    
+# Writer:
+with open("practice_writeto.csv",'w',newline='',encoding='utf-8') as f:
+    csv_data = csv.writer(f)
+    csv_data.writerows([['this','is','a','header'],['this','is','first','row']])
+# For loop vs CSV reader
+# For loop work similary with CSV header with appropriate seperator
+# For loop may run into issue when a field in csv has a seperator symbol inside the field
+# Example: CSV reader can read this but not loop: field1, "field2, still field2", field3
+# Using for loop by itself won't parse \n as end of line on Windows
+
